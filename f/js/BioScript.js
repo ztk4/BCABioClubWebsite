1f847a2a6f39538a16f0d16d024a8ab0cc2ac709
12fn <zacharytrent@gmail.com>
2012-09-16 13:08:30 -0400

    Added Content fade in/out and jQuery Library

--- f/js/BioScript.js
+++ f/js/BioScript.js
@@ -11,14 +11,27 @@ $(document).ready(function() {
 	});
 	$('.navbutton5Menu').hover(function() {
 		$('.navbutton5Menu')
-				.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 1</span></a>').fadeIn('slow'))
-				.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 2</span></a>').fadeIn('slow'))
-				.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 3</span></a>').fadeIn('slow'))
-				.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 4</span></a>').fadeIn('slow'))
-				.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 5</span></a>').fadeIn('slow'));
+			.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 1</span></a>').fadeIn('slow'))
+			.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 2</span></a>').fadeIn('slow'))
+			.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 3</span></a>').fadeIn('slow'))
+			.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 4</span></a>').fadeIn('slow'))
+			.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 5</span></a>').fadeIn('slow'));
 	}, function() {
 		$('.navbutton5Menu')
 			.empty()
 			.append('<a href="#" class="navlink"><span class="navbox navbutton5" style="width: 200px;">Important Information</span></a>');
 	});
-})
+    	$('#content').css("display", "none");
+ 
+	$('#content').fadeIn(2000);
+ 
+    	$('a').click(function(event){
+        	event.preventDefault();
+        	linkLocation = this.href;
+        	$('#content').fadeOut(1000, redirectPage);      
+    	});
+         
+    	function redirectPage() {
+        	window.location = linkLocation;
+    	}
+});
