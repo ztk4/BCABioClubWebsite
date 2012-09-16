0388b79523ca5bc89355f7e57aa983e2ce58734b
12fn <zacharytrent@gmail.com>
2012-09-16 12:07:34 -0400

    Basic Home (Navbar) + css w/ js to match

---
+++ f/js/BioScript.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('.navbutton3Menu').hover(function() {
+		$('.navbutton3Menu')
+			.append($('<br /><a href="#" class="navlink"><span class="subNavbox">Person 1</span></a>').fadeIn('slow'))
+			.append($('<br /><a href="#" class="navlink"><span class="subNavbox">Person 2</span></a>').fadeIn('slow'))
+			.append($('<br /><a href="#" class="navlink"><span class="subNavbox">Person 3</span></a>').fadeIn('slow'));
+	}, function() {
+		$('.navbutton3Menu')
+			.empty()
+			.append('<a href="#" class="navlink"><span class="navbox navbutton3">Contact</span></a>');
+	});
+	$('.navbutton5Menu').hover(function() {
+		$('.navbutton5Menu')
+				.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 1</span></a>').fadeIn('slow'))
+				.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 2</span></a>').fadeIn('slow'))
+				.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 3</span></a>').fadeIn('slow'))
+				.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 4</span></a>').fadeIn('slow'))
+				.append($('<br /><a href="#" class="navlink"><span class="subNavbox" style="width: 200px;">Info Link 5</span></a>').fadeIn('slow'));
+	}, function() {
+		$('.navbutton5Menu')
+			.empty()
+			.append('<a href="#" class="navlink"><span class="navbox navbutton5" style="width: 200px;">Important Information</span></a>');
+	});
+})
